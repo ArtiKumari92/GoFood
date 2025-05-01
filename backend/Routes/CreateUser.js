6e87099c78f3766432f3d377803ec7f36a2feefe
ArtiKumari92 <artiyadav574@gmail.com>
2025-05-02 00:32:18 +0530

    Latest code v8

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -2,6 +2,9 @@ const express = require('express')
 const User = require('../models/User')
 const router = express.Router()
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs')
+var jwt = require('jsonwebtoken');
+const jwtSecret = "HaHaaHaaHaaHaaHaaHaaHa"
 
 router.post("/createuser", [
     body('email').isEmail(),
@@ -15,14 +18,30 @@ router.post("/createuser", [
         return res.status(400).json({ success, errors: errors.array() })
     }
 
+    const salt = await bcrypt.genSalt(10)
+    let securePass = await bcrypt.hash(req.body.password, salt);
+
     try {
         await User.create({
             name: req.body.name,
-            password: req.body.password,
+            // password: req.body.password,  first write this and then use bcryptjs
+            password: securePass,
             email: req.body.email,
             location: req.body.location
+        }).then(user => {
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            const authToken = jwt.sign(data, jwtSecret);
+            success = true
+            res.json({ success, authToken })
         })
-        res.json({ success: true });
+            .catch(err => {
+                console.log(err);
+                res.json({ error: "Please enter a unique value." })
+            })
 
     } catch (error) {
         console.log(error);
@@ -48,15 +67,23 @@ router.post('/login', [
             return res.status(400).json({ success, error: "Try Logging in with correct credentials" });
         }
 
-        if(req.body.password !== user.password) {
-            return res.status(400).json({ errors: "Try Logging in with correct credentials" });
+        const pwdCompare = await bcrypt.compare(password, user.password); // this return true false.
+        if (!pwdCompare) {
+            return res.status(400).json({ success, error: "Try Logging in with correct credentials" });
         }
-            
-        return res.json({ success: true });
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        success = true;
+        const authToken = jwt.sign(data, jwtSecret);
+        res.json({ success, authToken })
+
 
     } catch (error) {
-        console.log(error)
-        res.json({ success: false });
+        console.error(error.message)
+        res.send("Server Error")
     }
 })
 
